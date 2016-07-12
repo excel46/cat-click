334cb28126ece40410ba52b371d5b33a41198cbf
Mittal <c016541@bank-dns.com>
2016-07-12 16:01:46 -0700

    Update for 2 cats with names and one count

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(function(){
 var clickCount=0;
-$('#catImgId').click(function (e){
+var images = [ {id :'idImg1', name:'cat1', src: "http://lorempixel.com/400/200/cats/1", clickCount:0}, {id :'idImg2', name:'cat2', src:"http://lorempixel.com/400/200/cats/2", clickCount:0}];
+
+var parentDiv= $('#idImageDiv');
+$(images).each(function (dex,item){
+	var imgDiv= $('<div>');
+	imgDiv.append($('<img>').attr('src', item.src).attr('id',dex));
+	imgDiv.append($('<div>').text('Cat name: '+item.name));
+	imgDiv.append($('<span>').text('Cat click count: '+item.clickCount));
+	//imgDiv.append('</div>');
+	imgDiv.appendTo(parentDiv);
+});
+$('img').click(function (e){
+	//images[this.attr('id')].clickCount++;
 	clickCount++;
 	updateView();
 });
