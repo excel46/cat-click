ba22eac75f4958240e7cf9cc7519c60a4507e668
Mittal <c016541@bank-dns.com>
2016-07-13 11:14:34 -0700

    convert the cat-click to angularjs/bootsrap

--- js/main.js
+++ js/main.js
@@ -1,24 +1,9 @@
-$(function(){
-var clickCount=0;
-var images = [ {id :'idImg1', name:'cat1', src: "http://lorempixel.com/400/200/cats/1", clickCount:0}, {id :'idImg2', name:'cat2', src:"http://lorempixel.com/400/200/cats/2", clickCount:0}];
+angular.module('catClick', []);
+angular.module('catClick').controller('catCtrl',[ '$scope', function ($scope){
 
-var parentDiv= $('#idImageDiv');
-$(images).each(function (dex,item){
-	var imgDiv= $('<div>');
-	imgDiv.append($('<img>').attr('src', item.src).attr('id',dex));
-	imgDiv.append($('<div>').text('Cat name: '+item.name));
-	//imgDiv.append($('<span>').text('Cat click count: '+item.clickCount));
-	//imgDiv.append('</div>');
-	imgDiv.appendTo(parentDiv);
-});
-$('img').click(function (e){
-	//images[this.attr('id')].clickCount++;
-	clickCount++;
-	updateView();
-});
-function updateView(){
-$('#idCount').text(clickCount);
-}
+$scope.catList= [ {id :'idImg1', name:'cat1', src: "http://lorempixel.com/400/200/cats/1", clickCount:0}, {id :'idImg2', name:'cat2', src:"http://lorempixel.com/400/200/cats/2", clickCount:0}];
 
-updateView();
-});
+$scope.imageClick= function (cat){
+cat.clickCount++;
+};
+}]);
