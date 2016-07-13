01d2886a1c54707adb7691598fae755a4b8a6216
Mittal <c016541@bank-dns.com>
2016-07-13 11:33:49 -0700

    Excercise3: show the catlist and enable selective show

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ angular.module('catClick', []);
 angular.module('catClick').controller('catCtrl',[ '$scope', function ($scope){
 
 $scope.catList= [ {id :'idImg1', name:'cat1', src: "http://lorempixel.com/400/200/cats/1", clickCount:0}, {id :'idImg2', name:'cat2', src:"http://lorempixel.com/400/200/cats/2", clickCount:0}];
+$scope.cat={};
 
 $scope.imageClick= function (cat){
 cat.clickCount++;
 };
+
+$scope.selectCat=function (cat){
+$scope.cat=cat;
+}
+
 }]);
