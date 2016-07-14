80fe915c345df14bdb79abaf92b552c46369afa7
Narain Mittal <mittalnarain123@gmail.com>
2016-07-14 16:06:10 -0700

    using MOV pattern and better looking app

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
 angular.module('catClick', []);
 angular.module('catClick').controller('catCtrl',[ '$scope', function ($scope){
 
-$scope.catList= [ {id :'idImg1', name:'cat1', src: "http://lorempixel.com/400/200/cats/1", clickCount:0}, {id :'idImg2', name:'cat2', src:"http://lorempixel.com/400/200/cats/2", clickCount:0}];
+var model={
+	catList:[],
+	
+	init: function (){
+		var catData=[];
+		catData.push({id :'catId1', name:'Mr. Tom', src: "http://lorempixel.com/400/200/cats/1", clickCount:0});
+		catData.push({id :'catId2', name:'Mr. Jerry', src: "http://lorempixel.com/400/200/cats/2", clickCount:0});
+		catData.push({id :'catId3', name:'Mumbo Jumbo', src: "http://lorempixel.com/400/200/cats/3", clickCount:0});
+		catData.push({id :'catId4', name:'Purr', src: "http://lorempixel.com/400/200/cats/4", clickCount:0});
+		catData.push({id :'catId5', name:'Kitty Kat', src: "http://lorempixel.com/400/200/cats/5", clickCount:0});
+		model.catList=catData;
+	}
+	
+};
+$scope.octopus={
+	
+	getAllCats: function (){
+		return model.catList;
+	},
+	selectCat: function (catId){
+		angular.forEach($scope.octopus.getAllCats(), function (catObj,dex){
+			if(catObj.id==catId){
+				$scope.octopus.selectedCat=catObj;
+			}
+		});
+		
+	},
+	selectedCat:{},
+	catClick: function (catId){
+		angular.forEach($scope.octopus.getAllCats(), function (catObj,dex){
+				if(catObj.id==catId){
+					catObj.clickCount++;
+					$scope.octopus.selectCat(catId);
+				}
+			});
+	},
+	init: function (){
+		model.init();
+	}
+};
+
+$scope.octopus.init();
+/*
+$scope.catListView={};
+
+$scope.catPreview ={};
+
+
+$scope.catList= [ {id :'idImg1', name:'Mr. Tom', src: "http://lorempixel.com/400/200/cats/1", clickCount:0}, {id :'idImg2', name:'Little Jerry', src:"http://lorempixel.com/400/200/cats/2", clickCount:0}, {id :'idImg2', name:'Mumbo Jumbo', src:"http://lorempixel.com/400/200/cats/3", clickCount:0}];
 $scope.cat={};
 
 $scope.imageClick= function (cat){
@@ -11,5 +59,6 @@ cat.clickCount++;
 $scope.selectCat=function (cat){
 $scope.cat=cat;
 }
+*/
 
 }]);
