78b55e051aca10ba78ab7d4672ededd3c24aa5fb
Narain Mittal <mittalnarain123@gmail.com>
2016-07-18 16:40:09 -0700

    knockout implementation with separate model

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
-var ViewModel = function (){
-
+var Cat= function (){
 this.catName=ko.observable('Mr. Jerry');
 this.catSrc=ko.observable('http://lorempixel.com/400/200/cats/1');
 this.clickCount=ko.observable(0);
-this.increment= function (){
-	this.clickCount(this.clickCount()+1);
-	if(this.clickCount()==5){
-		this.level('sitter');
-	}else if(this.clickCount()==10){
-		this.level('toddler');
-	}else if(this.clickCount()==15){
-		this.level('teen');
+this.level= ko.computed(function (){
+	var level;
+	if(this.clickCount()<5){
+		level= 'newborn';
+	}else if(this.clickCount()<10){
+		level='sitter';
+	}else if(this.clickCount()<15){
+		level='toddler';
+	}else{
+		level='teen';
 	}
-};
-this.level= ko.observable('newborn');
+	return level;
+	
+},this);
 
 this.catTitle= ko.computed(function (){
 	return this.catName()+' '+this.clickCount();
 }, this);
 
 this.nickNames= ko.observableArray(['Mr. Tom', 'Black Cat', 'Jumbo']);
+}
+
+var ViewModel = function (){
+
+this.currentCat= ko.observable(new Cat());
+this.increment= function (){
+	this.currentCat().clickCount(this.currentCat().clickCount()+1);
+	
+};
+
 }
 ko.applyBindings(new ViewModel());
