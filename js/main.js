5476c317dea2b6f0593bc2bccecddec2477acc72
Narain Mittal <mittalnarain123@gmail.com>
2016-07-18 16:31:10 -0700

    knockout implementation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-angular.module('catClick', []);
-angular.module('catClick').controller('catCtrl',[ '$scope', function ($scope){
+var ViewModel = function (){
 
-$scope.catList= [ {id :'idImg1', name:'cat1', src: "http://lorempixel.com/400/200/cats/1", clickCount:0}, {id :'idImg2', name:'cat2', src:"http://lorempixel.com/400/200/cats/2", clickCount:0}];
-
-$scope.imageClick= function (cat){
-cat.clickCount++;
+this.catName=ko.observable('Mr. Jerry');
+this.catSrc=ko.observable('http://lorempixel.com/400/200/cats/1');
+this.clickCount=ko.observable(0);
+this.increment= function (){
+	this.clickCount(this.clickCount()+1);
+	if(this.clickCount()==5){
+		this.level('sitter');
+	}else if(this.clickCount()==10){
+		this.level('toddler');
+	}else if(this.clickCount()==15){
+		this.level('teen');
+	}
 };
-}]);
+this.level= ko.observable('newborn');
+
+this.catTitle= ko.computed(function (){
+	return this.catName()+' '+this.clickCount();
+}, this);
+
+this.nickNames= ko.observableArray(['Mr. Tom', 'Black Cat', 'Jumbo']);
+}
+ko.applyBindings(new ViewModel());
